79e78a421d2bf7b2ead3d7d88a8a68029a5b0b64
matthewthekramer <kramer22@wisc.edu>
2018-12-26 18:41:33 -0600

    added interchanging rest + work timer resets, added color changes based on rest/work time, rest time color not working atm

--- src/components/TimerView.js
+++ src/components/TimerView.js
@@ -6,8 +6,13 @@ import {
   Text,
   Button
 } from 'react-native';
+import {
+  Card,
+  CardSection,
+} from './common';
 import Timer from './Timer';
 import TimerButton from './TimerButton';
+
 
 import {
   decrementSec,
@@ -19,7 +24,7 @@ import {
 class TimerView extends Component {
   constructor(props) {
     super(props);
-    this.props.initTimer({ minutes: 1, seconds: 10 });
+    this.props.initTimer({ minutes: 0, seconds: 15 });
     //this.interval = setInterval(
     //  () => this.setState((prevState) => ({ timer: prevState.timer - 1 })),
     //  1000
@@ -30,6 +35,15 @@ class TimerView extends Component {
   }
   onPausePress() {
     this.props.pauseTimer();
+  }
+  getContainerStyle() {
+    if (this.props.paused) {
+      return { ...styles.containerStyle, backgroundColor: '#c1c1c1' };
+    }
+    if (this.props.resting) {
+      return { ...styles.containerStyle, backgroundColor: '#990000' };
+    }
+    return { ...styles.containerStyle, backgroundColor: '#00FF00' };
   }
   renderTimerButton() {
     if (this.props.paused) {
@@ -47,17 +61,39 @@ class TimerView extends Component {
   }
   render() {
     return (
-      <View>
-        <Timer minutes={this.props.minutes} seconds={this.props.seconds} />
+      <View style={this.getContainerStyle()}>
+        <Timer minutes={this.props.curMinutes} seconds={this.props.curSeconds} />
         {this.renderTimerButton()}
       </View>
     );
   }
 }
 const mapStateToProps = state => {
-  const { minutes, seconds, paused } = state.timer;
+  const {
+    curMinutes,
+    curSeconds,
+    paused,
+    roundTime,
+    restTime
+  } = state.timer;
 
-  return { minutes, seconds, paused };
+  return {
+    curMinutes,
+    curSeconds,
+    paused,
+    roundTime,
+    restTime,
+  };
+};
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    backgroundColor: '#c1c1c1',
+  },
+  sectionStyle: {
+    backgroundColor: '#FF0000',
+  }
 };
 
 export default connect(mapStateToProps, {
--- src/reducers/TimerReducer.js
+++ src/reducers/TimerReducer.js
@@ -6,30 +6,69 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-  seconds: 0,
-  minutes: 0,
-  done: false,
+  curSeconds: 0, //seconds remaining until the round/rest period is over
+  curMinutes: 0, //minutes remaining until the round/rest period is over
   paused: true,
+  resting: false, //if currently during the rest time
+  warning: false, //if small time remaining during work period
   intervalID: 0,
+
+  roundTime: {
+    minutes: 0,
+    seconds: 0,
+  },
+  restTime: {
+    minutes: 0,
+    seconds: 8,
+  },
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case DECREMENT_SEC:
-      if (state.seconds === 0) {
-        if (state.minutes !== 0) {
-          return { ...state, seconds: 60, minutes: state.minutes - 1 };
+      if (state.curSeconds === 0) {
+        if (state.curMinutes !== 0) {
+          return { ...state, curSeconds: 60, curMinutes: state.curMinutes - 1 };
         }
-        return { ...state, done: true };
+        //out of time
+        //if the period that just ran out was a rest period
+        if (state.resting) {
+          return {
+            ...state,
+            curSeconds: state.roundTime.seconds,
+            curMinutes: state.roundTime.minutes,
+            resting: false,
+          };
+        }
+        //if the period that just ran out was a work period
+        return {
+          ...state,
+          curSeconds: state.restTime.seconds,
+          curMinutes: state.restTime.minutes,
+          resting: true,
+          warning: false,
+        };
+      //if we should set warning
+      } else if (!state.resting && state.curSeconds + (state.curMinutes * 60)) {
+        return {
+          ...state,
+          curSeconds: state.curSeconds - 1,
+          warning: true,
+       };
       }
-      return { ...state, seconds: state.seconds - 1 };
+      //if not out of time, just decrement normally
+      return { ...state, curSeconds: state.curSeconds - 1 };
     case INIT_TIMER:
       return {
         ...state,
         done: false,
-        seconds: action.payload.seconds,
-        minutes: action.payload.minutes,
-      };
+        curSeconds: action.payload.seconds,
+        curMinutes: action.payload.minutes,
+        roundTime: {
+          seconds: action.payload.seconds,
+          minutes: action.payload.minutes,
+        }
+      };//TODO init rest
     //sets interval id and unpauses timer
     case PLAY_TIMER:
       return {
