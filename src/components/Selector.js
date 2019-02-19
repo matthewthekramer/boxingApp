5c4e820ac18508b4f2a34a97d662abe130f9c35a
matthewthekramer <kramer22@wisc.edu>
2019-02-19 13:46:48 -0600

    styled selector

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,40 +2,74 @@
 
 import React from 'react';
 import {
-  TouchableHighlight,
+  TouchableOpacity,
   View,
+  Text,
 } from 'react-native';
 
-const Selector = (props) => {
-  console.log('in selector', props.selected);
-  return (
-    <TouchableHighlight onPress={props.onPress} >
-      <View style={[styles.container, props.style]} >
-        {
-          props.selected > 0 ?
-            <View style={styles.innerContainer} />
-            : null
-        }
+const renderButtonContent = (selected) => {
+  if (selected === 0) {
+    return (
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Select</Text>
       </View>
-    </TouchableHighlight>
+    );
+  } else {
+    return (
+      <View style={styles.numberContainer}>
+        <Text style={styles.number}>{selected}</Text>
+      </View>
+    );
+  }
+};
+
+const Selector = (props) => {
+  return (
+    <View style={[styles.container, props.style]} >
+      <TouchableOpacity style={styles.opacity} onPress={props.onPress}>
+        {renderButtonContent(props.selected)}
+      </TouchableOpacity>
+    </View>
   );
 };
+
 const styles = {
   container: {
-    height: 24,
-    width: 24,
-    borderRadius: 12,
-    borderWidth: 2,
-    borderColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
-  innerContainer: {
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    backgroundColor: '#000',
+  text: {
   },
+  textContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderBottomWidth: 2,
+    borderColor: '#000F0F',
+    width: '100%',
+    backgroundColor: 'rgba(255,220,220,.3)',
+  },
+  opacity: {
+    width: '100%',
+  },
+  number: {
+    width: '100%',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderBottomWidth: 2,
+    borderColor: '#000F0F',
+    borderRadius: 10,
+    color: '#000F0',
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(200,20,20,.2)',
+  },
+  numberContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+
+  }
 };
 
 export default Selector;
